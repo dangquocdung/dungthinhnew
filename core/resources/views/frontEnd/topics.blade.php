90d3cbcfa2916bdff29c1d39a7b7d0691de64d24
dangquocdung <dungthinhvn@gmail.com>
2020-04-24 14:21:01 +0700

    asdsad

--- core/resources/views/frontEnd/topics.blade.php
+++ core/resources/views/frontEnd/topics.blade.php
@@ -392,7 +392,9 @@
                                             @endif
                                             {{--End of -- Additional Feilds--}}
 
-                                            <p>{!! strip_tags($Topic->$details) !!}</p>
+                                            <p>{!! mb_substr(strip_tags($Topic->$details),0, 300)."..." !!}</p>
+
+                                            
                                             <div class="bottom-article">
                                                 <ul class="meta-post">
                                                     @if($Topic->webmasterSection->date_status)
