bc6f44fe6814456e2e08038f6aa41d020cc81634
dangquocdung <dungthinhvn@gmail.com>
2020-04-28 15:19:46 +0700

    ádsa

--- core/resources/views/frontEnd/includes/foot.blade.php
+++ core/resources/views/frontEnd/includes/foot.blade.php
@@ -123,6 +123,20 @@
     {!! $WebmasterSettings->google_analytics_code !!}
 @endif
 
+<script>
+    if ('serviceWorker' in navigator ) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+</script>
+
 <?php
 if ($PageTitle == "") {
     $PageTitle = Helper::GeneralSiteSettings("site_title_" . @Helper::currentLanguage()->code);
