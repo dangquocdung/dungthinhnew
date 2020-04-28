0eaeaf4733947481d8213a0ae98d22db26d7924d
dangquocdung <dungthinhvn@gmail.com>
2020-04-28 12:08:26 +0700

    ádsad

---
+++ core/resources/views/frontEnd/includes/foot-2.blade.php
@@ -0,0 +1,161 @@
+<a href="#" title="{{ __('frontend.toTop') }}" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+
+<script type="text/javascript">
+    var page_dir = "{{ @Helper::currentLanguage()->direction }}";
+</script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+<script src="{{ URL::asset('frontEnd/js/jquery.easing.1.3.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/jquery.fancybox.pack.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/jquery.fancybox-media.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/google-code-prettify/prettify.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/portfolio/jquery.quicksand.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/portfolio/setting.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/jquery.flexslider.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/flipbook.min.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/animate.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/custom.js') }}"></script>
+<script src="{{ URL::asset('frontEnd/js/owl-carousel/owl.carousel.js') }}"></script>
+{{--ajax subscribe to news letter--}}
+@if(Helper::GeneralSiteSettings("style_subscribe"))
+    <script type="text/javascript">
+        jQuery(document).ready(function ($) {
+            "use strict";
+
+            //Subscribe
+            $('form.subscribeForm').submit(function () {
+
+                var f = $(this).find('.form-group'),
+                    ferror = false,
+                    emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+
+                f.children('input').each(function () { // run all inputs
+
+                    var i = $(this); // current input
+                    var rule = i.attr('data-rule');
+
+                    if (rule !== undefined) {
+                        var ierror = false; // error flag for current input
+                        var pos = rule.indexOf(':', 0);
+                        if (pos >= 0) {
+                            var exp = rule.substr(pos + 1, rule.length);
+                            rule = rule.substr(0, pos);
+                        } else {
+                            rule = rule.substr(pos + 1, rule.length);
+                        }
+
+                        switch (rule) {
+                            case 'required':
+                                if (i.val() === '') {
+                                    ferror = ierror = true;
+                                }
+                                break;
+
+                            case 'minlen':
+                                if (i.val().length < parseInt(exp)) {
+                                    ferror = ierror = true;
+                                }
+                                break;
+
+                            case 'email':
+                                if (!emailExp.test(i.val())) {
+                                    ferror = ierror = true;
+                                }
+                                break;
+
+                            case 'checked':
+                                if (!i.attr('checked')) {
+                                    ferror = ierror = true;
+                                }
+                                break;
+
+                            case 'regexp':
+                                exp = new RegExp(exp);
+                                if (!exp.test(i.val())) {
+                                    ferror = ierror = true;
+                                }
+                                break;
+                        }
+                        i.next('.validation').html('<i class=\"fa fa-info\"></i> &nbsp;' + ( ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '' )).show();
+                        !ierror ? i.next('.validation').hide() : i.next('.validation').show();
+                    }
+                });
+                if (ferror) return false;
+                else var str = $(this).serialize();
+                $.ajax({
+                    type: "POST",
+                    url: "{{ route("subscribeSubmit") }}",
+                    data: str,
+                    success: function (msg) {
+                        if (msg == 'OK') {
+                            $("#subscribesendmessage").addClass("show");
+                            $("#subscribeerrormessage").removeClass("show");
+                            $("#subscribe_name").val('');
+                            $("#subscribe_email").val('');
+                        }
+                        else {
+                            $("#subscribesendmessage").removeClass("show");
+                            $("#subscribeerrormessage").addClass("show");
+                            $('#subscribeerrormessage').html(msg);
+                        }
+
+                    }
+                });
+                return false;
+            });
+
+        });
+    </script>
+@endif
+
+
+
+<!-- Prevent Right Click (noscript) -->
+<script language="JavaScript">
+  /**
+    * Disable right-click of mouse, F12 key, and save key combinations on page
+    * By Arthur Gareginyan (https://www.arthurgareginyan.com)
+    * For full source code, visit https://mycyberuniverse.com
+    */
+  window.onload = function() {
+    document.addEventListener("contextmenu", function(e){
+      e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(e) {
+      // "I" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        disabledEvent(e);
+      }
+      // "J" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        disabledEvent(e);
+      }
+      // "S" key + macOS
+      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+      }
+      // "U" key
+      if (e.ctrlKey && e.keyCode == 85) {
+        disabledEvent(e);
+      }
+      // "F12" key
+      if (event.keyCode == 123) {
+        disabledEvent(e);
+      }
+    }, false);
+    function disabledEvent(e){
+      if (e.stopPropagation){
+        e.stopPropagation();
+      } else if (window.event){
+        window.event.cancelBubble = true;
+      }
+      e.preventDefault();
+      return false;
+    }
+  };
+</script>
+
+
