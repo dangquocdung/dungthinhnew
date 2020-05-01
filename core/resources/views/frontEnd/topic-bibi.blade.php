528f9442f7210a38a849cbf5f4b8000b3ce37a85
dangquocdung <dungthinhvn@gmail.com>
2020-05-01 19:30:36 +0700

    asdsad

--- core/resources/views/frontEnd/topic-bibi.blade.php
+++ core/resources/views/frontEnd/topic-bibi.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div id="bibi-info">
-    <h1>This E-Book is Published on the Web with BiB/i</h1>
+    <h1>This E-Book is Published on the Web with BiBi</h1>
     <ul>
         <li><a href="http://bibi.epub.link">BiB/i | EPUB Reader on your website. (Official Website / Japanese)</a></li>
         <li><a href="https://github.com/satorumurmur/bibi">BiB/i on GitHub (English)</a></li>
