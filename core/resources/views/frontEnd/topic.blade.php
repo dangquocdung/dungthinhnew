d655a744db00e938cf2c3baf8c7ccaf625d47197
dangquocdung <dungthinhvn@gmail.com>
2020-05-01 23:25:10 +0700

    xcxxc

--- core/resources/views/frontEnd/topic.blade.php
+++ core/resources/views/frontEnd/topic.blade.php
@@ -488,9 +488,9 @@
                                             &nbsp;{{ __('frontend.downloadAttach') }}</strong>
                                     </a>
 
-                                    <div class="embed-responsive embed-responsive-16by9">
+                                    <!-- <div class="embed-responsive embed-responsive-16by9">
                                         <iframe class="embed-responsive-item" src="{{ URL::to('doc-sach/bibi/?book='.$Topic->attach_file) }}" frameborder="0" height="480px" width="100%"></iframe>
-                                    </div>
+                                    </div> -->
 
                                 </div>
                                 @endif
