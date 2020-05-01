4cf57653ce70b45be8e431541990477ad4387647
dangquocdung <dungthinhvn@gmail.com>
2020-05-01 23:15:05 +0700

    xcxc

--- core/resources/views/frontEnd/topic.blade.php
+++ core/resources/views/frontEnd/topic.blade.php
@@ -487,6 +487,11 @@
                                             {!! Helper::GetIcon(URL::to('uploads/topics/'),$Topic->attach_file) !!}
                                             &nbsp;{{ __('frontend.downloadAttach') }}</strong>
                                     </a>
+
+                                    <div class="embed-responsive embed-responsive-16by9">
+                                        <iframe class="embed-responsive-item" src="{{ URL::to('doc-sach/bibi/?book='.$Topic->attach_file) }}" frameborder="0" height="480px" width="100%"></iframe>
+                                    </div>
+
                                 </div>
                                 @endif
 
